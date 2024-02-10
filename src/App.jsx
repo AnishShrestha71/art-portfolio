bd69d255c6b86035eafad300e66b3e7dd7ffac9a
Anish Shrestha <anishshrestha71@gmail.com>
2024-02-10 19:21:17 +0545

    router test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Link, Route, HashRouter as Router, Routes } from "react-router-dom";
 import About from "./assets/compnents/about";
 
 function App() {
