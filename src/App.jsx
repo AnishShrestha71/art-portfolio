2998814c941a1a268f4052ecb8384b04179c68f6
Anish Shrestha <anishshrestha71@gmail.com>
2024-02-10 19:14:17 +0545

    router test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import About from "./assets/compnents/about";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/about">about</Link>
+            </li>
+          </ul>
+        </div>
+        <Routes>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -21,15 +35,13 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          
-        </p>
+        <p></p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
