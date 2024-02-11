3996da5588b65084732187494b020825c8e2de8b
Anish Shrestha <anishshrestha71@gmail.com>
2024-02-11 20:38:39 +0545

    install tailwind and test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Link, Route, HashRouter as Router, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import About from "./assets/compnents/about";
 
 function App() {
@@ -10,27 +10,23 @@ function App() {
 
   return (
     <>
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/about">about</Link>
-            </li>
-          </ul>
-        </div>
+      <header>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Arts</li>
+          <li>Contact</li>
+          <li>
+            <Link to="/about">about</Link>
+          </li>
+        </ul>
         <Routes>
           <Route path="/about" element={<About />} />
         </Routes>
-      </Router>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      </header>
+
       <h1>deploy changes</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -40,6 +36,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div style={{ height: "450px" }}></div>
+      <div id="skills" style={{ height: "145px", backgroundColor: "red" }}>
+        body section
+      </div>
     </>
   );
 }
