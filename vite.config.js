6692acf9749f09f0046c43d3dad4010f1226051e
Anish Shrestha <anishshrestha71@gmail.com>
2024-03-04 13:02:02 +0545

    homepage change

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// art-portfolio/
 export default defineConfig({
   plugins: [react()],
-  base: "/art-portfolio/"
+  base: "/"
 })
